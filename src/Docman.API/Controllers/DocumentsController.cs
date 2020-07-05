93b3cbaac8ddcb4e46c3e2b5bc5bf88631377263
Pavlo Goncharenko <pashaghnp@gmail.com>
2020-07-05 12:27:29 +0300

    Add Docman.API project

---
+++ src/Docman.API/Controllers/DocumentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Docman.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly ILogger<DocumentsController> _logger;
+
+        public DocumentsController(ILogger<DocumentsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
