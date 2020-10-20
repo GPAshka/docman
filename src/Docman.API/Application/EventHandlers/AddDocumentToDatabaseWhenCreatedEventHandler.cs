04edfe970cc9972451ee8d56fda19af1ce6f59a8
Pavlo Goncharenko <pashaghnp@gmail.com>
2020-10-20 10:11:50 +0300

    Log exceptions in the AddDocumentToDatabaseWhenCreatedEventHandler

--- src/Docman.API/Application/EventHandlers/AddDocumentToDatabaseWhenCreatedEventHandler.cs
+++ src/Docman.API/Application/EventHandlers/AddDocumentToDatabaseWhenCreatedEventHandler.cs
@@ -2,23 +2,33 @@ using System.Threading;
 using System.Threading.Tasks;
 using Docman.API.Application.Dto.Events;
 using Docman.Infrastructure.Repositories;
+using LanguageExt;
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Unit = LanguageExt.Unit;
 
 namespace Docman.API.Application.EventHandlers
 {
     public class AddDocumentToDatabaseWhenCreatedEventHandler : INotificationHandler<DocumentCreatedEventDto>
     {
         private readonly DocumentRepository.AddDocument AddDocument;
+        private readonly ILogger<AddDocumentToDatabaseWhenCreatedEventHandler> _logger;
 
-        public AddDocumentToDatabaseWhenCreatedEventHandler(DocumentRepository.AddDocument addDocument)
+        public AddDocumentToDatabaseWhenCreatedEventHandler(DocumentRepository.AddDocument addDocument,
+            ILogger<AddDocumentToDatabaseWhenCreatedEventHandler> logger1)
         {
             AddDocument = addDocument;
+            _logger = logger1;
         }
 
         public async Task Handle(DocumentCreatedEventDto notification, CancellationToken cancellationToken)
         {
-            // TODO handle and log exceptions
-            await AddDocument(notification.Id, notification.Number, notification.Description);
+            TryAsync<Unit> handle = async () =>
+                await AddDocument(notification.Id, notification.Number, notification.Description).ToUnit();
+
+            await handle.Match(
+                Succ: u => { },
+                Fail: ex => _logger.LogError($"Error while handling {nameof(DocumentCreatedEventDto)}: {ex}"));
         }
     }
 }
